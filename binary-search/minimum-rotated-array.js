8afcfcd42e7962fbabcc08a2f828e5c685d18c0d
bikraman <bikramankarmakar10@gmail.com>
2024-10-02 00:55:34 +0530

    solved minimum rotated array using binary search

--- binary-search/minimum-rotated-array.js
+++ binary-search/minimum-rotated-array.js
@@ -4,15 +4,33 @@
  */
 var findMin = function(nums) {
 
-    for(let i = 0; i < nums.length - 1; i++) {
-        if(nums[i] > nums[i + 1]) {
-            console.log("min element: " + nums[i + 1])
-            return nums[i + 1]
-        }
+    if (nums[0] < nums[nums.length - 1]) {
+        return nums[0]
     }
 
-    console.log("min element: " + nums[0])
-    return nums[0]
+    let start = 0
+    let end = nums.length - 1
+
+    let pivot = Math.floor((start + end)/2)
+
+    let flag = true
+
+    while(flag) {
+
+        if (nums[pivot] > nums[pivot + 1]) {
+            flag = false
+            return nums[pivot + 1]
+        }
+
+        if (nums[pivot] > nums[start]) {
+            start = pivot
+        }
+        else {
+            end = pivot
+        }
+
+        pivot = Math.floor((start + end)/2)
+    }
     
 };
 
